77963e0b141de9efa0a0389641764ae90e06fb69
truwsfff <138609244+truwsfff@users.noreply.github.com>
2025-01-20 21:42:08 +0300

    in t

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import pygame
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((1920, 1080))
+    pygame.display.set_caption('1234567890')
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/window_manager.py
@@ -0,0 +1,12 @@
+class WindowManager:
+    def __init__(self, screen):
+        pass
+
+    def add_window(self):
+        pass
+
+    def set_window(self):
+        pass
+
+    def run(self):
+        pass

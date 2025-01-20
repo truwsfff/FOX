4d7f3a51667f7aebcec39a613ea8abd3736baa4d
truwsfff <138609244+truwsfff@users.noreply.github.com>
2025-01-20 23:27:46 +0300

    changes in data and in src

--- src/registration.py
+++ src/registration.py
@@ -1,4 +1,5 @@
 import pygame
+
 from src.window import Window
 
 
@@ -6,9 +7,17 @@ class Registration(Window):
     def __init__(self, screen):
         super().__init__(screen)
         pygame.display.set_caption('Регистрация')
-        self.surf = pygame.Rect(0, 0, 1920, 1080)
+        self.rects = {'surf': pygame.Rect(0, 0, 1920, 1080),
+                      'fox': pygame.Rect(225, 140, 256, 285)
+                      }
 
-        self.image = pygame.image.load('../data/window_login.png')
+        self.image_1 = pygame.image.load('../data/window_login.png')
+        pygame.transform.scale(self.image_1, (
+            self.rects.get('surf').width, self.rects.get('surf').height))
+        self.image_2 = pygame.image.load('../data/fox.png')
+        pygame.transform.scale(self.image_1, (
+            self.rects.get('surf').width, self.rects.get('surf').height))
 
     def draw(self):
-        self.screen.blit(self.image)
+        self.screen.blit(self.image_1, self.rects.get('surf').topleft)
+        self.screen.blit(self.image_2, self.rects.get('fox').topleft)

14b6953d70e7000cb395753a3d8132a10561964c
truwsfff <138609244+truwsfff@users.noreply.github.com>
2025-01-20 22:23:03 +0300

    changes in src

---
+++ src/registration.py
@@ -0,0 +1,6 @@
+import pygame
+
+
+class Registration:
+    def __init__(self):
+        pygame.display.set_caption('Регистрация')
--- src/window_manager.py
+++ src/window_manager.py
@@ -1,12 +1,17 @@
+from src.registration import Registration
+
+
 class WindowManager:
     def __init__(self, screen):
-        pass
+        self.screen = screen
+        self.windows = {
+            'registration': Registration(screen)
+        }
+        self.current_window = None
 
-    def add_window(self):
-        pass
-
-    def set_window(self):
-        pass
+    def set_window(self, name):
+        self.current_window = self.windows.get(name)
 
     def run(self):
-        pass
+        while self.current_window:
+            self.current_window.run()

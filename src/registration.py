eb2f60f5eaeaf1100965a3fb9a4388d5d264954c
truwsfff <138609244+truwsfff@users.noreply.github.com>
2025-01-21 08:22:26 +0300

    added some widgets in registration

--- src/registration.py
+++ src/registration.py
@@ -8,16 +8,45 @@ class Registration(Window):
         super().__init__(screen)
         pygame.display.set_caption('Регистрация')
         self.rects = {'surf': pygame.Rect(0, 0, 1920, 1080),
-                      'fox': pygame.Rect(225, 140, 256, 285)
+                      'fox': pygame.Rect(240, 140, 256, 285),
+                      'text_surf_login': pygame.Rect(480, 420, 312, 67),
+                      'text_surf_passw': pygame.Rect(480, 520, 312, 67)
                       }
 
         self.image_1 = pygame.image.load('../data/window_login.png')
         pygame.transform.scale(self.image_1, (
             self.rects.get('surf').width, self.rects.get('surf').height))
+
         self.image_2 = pygame.image.load('../data/fox.png')
         pygame.transform.scale(self.image_1, (
             self.rects.get('surf').width, self.rects.get('surf').height))
 
+        self.image_3 = pygame.image.load('../data/surf_for_text.png')
+        pygame.transform.scale(self.image_3, (
+            self.rects.get('text_surf_login').width,
+            self.rects.get('text_surf_login').height))
+
+        self.image_4 = pygame.image.load('../data/surf_for_text.png')
+        pygame.transform.scale(self.image_4, (
+            self.rects.get('text_surf_passw').width,
+            self.rects.get('text_surf_passw').height))
+
+        self.font = pygame.font.Font('../data/HomeVideo-Regular.otf', 36)
+        self.text_surface_login = self.font.render('Логин', True,
+                                                   pygame.Color('white'))
+        self.text_surface_passw = self.font.render('Пароль', True,
+                                                   pygame.Color('white'))
+
+        self.text_position_login = (575, 435)
+        self.text_position_passw = (480, 535)
+
     def draw(self):
         self.screen.blit(self.image_1, self.rects.get('surf').topleft)
         self.screen.blit(self.image_2, self.rects.get('fox').topleft)
+        self.screen.blit(self.image_3,
+                         self.rects.get('text_surf_login').topleft)
+        self.screen.blit(self.image_4,
+                         self.rects.get('text_surf_passw').topleft)
+
+        self.screen.blit(self.text_surface_login, self.text_position_login)
+        self.screen.blit(self.text_surface_passw, self.text_position_passw)
